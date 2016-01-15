c7c0feb8fbaa23832c7aecfb25b45f2df09820af
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-15 09:55:22 +0100

    - writing test to make sure pdf confirmation is generated - improve jrxml format to show more good looking confirmation

--- confirmation-sender/src/test/java/com/trading/PdfConfirmationGenerationIntegrationTest.java
+++ confirmation-sender/src/test/java/com/trading/PdfConfirmationGenerationIntegrationTest.java
@@ -1,4 +1,38 @@
 package com.trading;
 
+import com.google.common.io.Resources;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
+import org.junit.Test;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PdfConfirmationGenerationIntegrationTest {
+
+    @Test
+    public void pdf_confirmation_should_be_easily_generated_from_jrxml_template() throws Exception {
+        URL jrxmlTemplate = Resources.getResource("Confirmation.jrxml");
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(jrxmlTemplate.getFile());
+
+        byte[] data = JasperRunManager.runReportToPdf(jasperReport, parameters(), new JREmptyDataSource());
+
+        assertThat(data.length).isGreaterThan(0);
+        Files.write(Paths.get("Test-Confirmation.pdf"), data);
+    }
+
+    private Map<String, Object> parameters() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("ALLOC_RPT_ID", "1234");
+        map.put("TRANS_TYPE", "BUY");
+        return map;
+    }
 }
