66ce9af040e6e7363e29074503ab7d34e57bd3b5
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-16 08:46:00 +0100

    - update gradle wrapper to 2.10 - update gigaspaces to 10.2.1 - add skeleton project for confirmation REST service

---
+++ confirmation-service/src/main/java/com/trading/ConfirmationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfirmationServiceApplication.class, args);
+    }
+}
