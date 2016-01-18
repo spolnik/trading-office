7e1af224524adf1c8c4f3a71081ef18129c61c0e
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-18 21:32:51 +0100

    - move away of gigaspaces - bring jms/activemq in those places

--- confirmation-sender/src/main/java/com/trading/ReceivedAllocationReportMessageListener.java
+++ confirmation-sender/src/main/java/com/trading/ReceivedAllocationReportMessageListener.java
@@ -1,60 +1,52 @@
 package com.trading;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 import net.sf.jasperreports.engine.*;
-import org.openspaces.core.GigaSpace;
-import org.openspaces.events.EventDriven;
-import org.openspaces.events.EventTemplate;
-import org.openspaces.events.TransactionalEvent;
-import org.openspaces.events.adapter.SpaceDataEvent;
-import org.openspaces.events.notify.NotifyType;
-import org.openspaces.events.polling.Polling;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-@EventDriven
-@Polling
-@NotifyType(write = true, update = true)
-@TransactionalEvent
-public class ReceivedAllocationReportListener {
+public class ReceivedAllocationReportMessageListener {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ReceivedAllocationReportListener.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ReceivedAllocationReportMessageListener.class);
 
     private final JasperReport jasperReport;
     private final Sender<Confirmation> confirmationSender;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public ReceivedAllocationReportListener(Sender<Confirmation> confirmationSender) throws JRException {
+    private ConfigurableApplicationContext context;
+
+    @Autowired
+    public ReceivedAllocationReportMessageListener(Sender<Confirmation> confirmationSender) throws JRException {
         this.confirmationSender = confirmationSender;
         URL jrxmlTemplate = Resources.getResource("Confirmation.jrxml");
 
         jasperReport = JasperCompileManager.compileReport(jrxmlTemplate.getFile());
+        objectMapper = new ObjectMapper();
     }
 
-    @EventTemplate
-    AllocationReport unprocessedData() {
-        AllocationReport template = new AllocationReport();
-        template.setMessageStatus(MessageStatus.NEW);
-        return template;
-    }
-
-    @SpaceDataEvent
-    public void eventListener(AllocationReport allocationReport, GigaSpace space) {
-        LOG.info("Retrieved from cache: " + allocationReport);
+    @JmsListener(destination = "incoming.allocation.report.queue", containerFactory = "jmsContainerFactory")
+    public void eventListener(String message) throws IOException {
+        AllocationReport allocationReport = objectMapper.readValue(message, AllocationReport.class);
+        LOG.info("Received: " + allocationReport);
 
         try {
             byte[] data = JasperRunManager.runReportToPdf(
                     jasperReport, parameters(allocationReport), new JREmptyDataSource()
             );
 
-            updateStateOfAllocationAndSaveIntoGigaspaces(allocationReport, space);
+            allocationReport.setMessageStatus(MessageStatus.SENT);
 
             Confirmation confirmation = createConfirmationBasedOn(allocationReport, data);
             confirmationSender.send(confirmation);
@@ -62,11 +54,6 @@ public class ReceivedAllocationReportListener {
         } catch (JRException e) {
             LOG.error(e.getMessage(), e);
         }
-    }
-
-    private void updateStateOfAllocationAndSaveIntoGigaspaces(AllocationReport allocationReport, GigaSpace space) {
-        allocationReport.setMessageStatus(MessageStatus.SENT);
-        space.write(allocationReport);
     }
 
     private Confirmation createConfirmationBasedOn(AllocationReport allocationReport, byte[] data) {
