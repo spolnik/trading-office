53d10d654d398d063900fc9ba8d3e58502362cdf
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-18 22:08:45 +0100

    - make apps running on different ports - fix issue with incompatible jackson 2.7.0 and spring 4.2.4 (use 2.6.3) -

--- confirmation-sender/src/main/java/com/trading/ConfirmationSenderApplication.java
+++ confirmation-sender/src/main/java/com/trading/ConfirmationSenderApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 
 import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
+@EnableJms
+@PropertySource("classpath:app.properties")
 public class ConfirmationSenderApplication implements CommandLineRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConfirmationSenderApplication.class);
