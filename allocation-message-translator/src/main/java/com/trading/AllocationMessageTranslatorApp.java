749f3e88b7d8f8580fce97b91d78277d2be13022
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-19 18:01:54 +0100

    - remove unused imports

--- allocation-message-translator/src/main/java/com/trading/AllocationMessageTranslatorApp.java
+++ allocation-message-translator/src/main/java/com/trading/AllocationMessageTranslatorApp.java
@@ -1,10 +1,7 @@
 package com.trading;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
