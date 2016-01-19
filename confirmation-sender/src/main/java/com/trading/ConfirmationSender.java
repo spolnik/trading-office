11582f14ecd6928d35f618db3e63c6ea3dbf1587
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-19 19:07:04 +0100

    - fix issue with hardcoded localhost address for confirmation service

--- confirmation-sender/src/main/java/com/trading/ConfirmationSender.java
+++ confirmation-sender/src/main/java/com/trading/ConfirmationSender.java
@@ -2,16 +2,22 @@ package com.trading;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class ConfirmationSender implements Sender<Confirmation> {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConfirmationSender.class);
 
+    @Value("${confirmationServiceUrl}")
+    private String confirmationServiceUrl;
+
     @Override
     public void send(Confirmation confirmation) {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject("http://localhost:9000/api/confirmation", confirmation, Object.class);
+        restTemplate.postForObject(confirmationServiceUrl, confirmation, Object.class);
 
         LOG.info("Confirmation sent: " + confirmation);
     }
