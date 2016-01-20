dfa1eb4f8115a7ad49090e9be76b62b80f217457
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-20 21:10:14 +0100

    - add integration tests to cover new rest services

---
+++ finance-data-service/src/test/java/FinanceDataServiceIntegrationTest.java
@@ -0,0 +1,29 @@
+import com.trading.FinanceDataApplication;
+import com.trading.Instrument;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FinanceDataServiceIntegrationTest {
+
+    @Before
+    public void setUp() throws Exception {
+        FinanceDataApplication.main(new String[0]);
+    }
+
+    @Test
+    public void service_returns_data_for_intel_symbol() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Instrument instrument = restTemplate.getForObject(
+                "http://localhost:9004/api/instrument/INTC",
+                Instrument.class
+        );
+
+        assertThat(instrument.getName()).isEqualTo("Intel Corporation Stocks");
+        assertThat(instrument.getExchange()).isEqualTo("NMS");
+        assertThat(instrument.getCurrency()).isEqualTo("USD");
+        assertThat(instrument.getSymbol()).isEqualTo("INTC");
+    }
+}
