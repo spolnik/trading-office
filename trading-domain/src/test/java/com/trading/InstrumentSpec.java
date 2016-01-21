e0a59e6c7b85e6467ae14cfd4466fad101678a2c
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-21 08:44:13 +0100

    - add json serialization tests for Instrument

---
+++ trading-domain/src/test/java/com/trading/InstrumentSpec.java
@@ -0,0 +1,39 @@
+package com.trading;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InstrumentSpec {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void can_be_parsed_to_json_and_converted_back_to_object() throws Exception {
+
+        String instrumentAsJson = objectMapper.writeValueAsString(
+                instrument()
+        );
+
+        Instrument instrument = objectMapper.readValue(
+                instrumentAsJson, Instrument.class
+        );
+
+        assertThat(instrument).isEqualToComparingFieldByField(instrument());
+    }
+
+    private Instrument instrument() {
+        Instrument instrument = new Instrument();
+
+        instrument.setCurrency("USD");
+        instrument.setExchange("Nasdaq");
+        instrument.setName("Amazon Stocks");
+        instrument.setSymbol("AMZN");
+        instrument.setPrice(BigDecimal.TEN);
+
+        return instrument;
+    }
+}
