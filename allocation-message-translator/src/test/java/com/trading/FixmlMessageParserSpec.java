fce06ee91789b036a8d61ff2aafe1854683569f1
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-21 20:02:23 +0100

    - confirmation sender integration test - replace remote instance of activemq with embedded broker

--- allocation-message-translator/src/test/java/com/trading/FixmlMessageParserSpec.java
+++ allocation-message-translator/src/test/java/com/trading/FixmlMessageParserSpec.java
@@ -3,21 +3,25 @@ package com.trading;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FixmlMessageParserSpec {
 
     private static AllocationReport allocationReport;
 
+    private static final String ALLOCATION_REPORT_ID = UUID.randomUUID().toString();
+
     @BeforeClass
     public static void setUp() throws Exception {
         FixmlMessageParser parser = new FixmlMessageParser();
-        allocationReport = parser.parse(TestData.FIXML_ALLOCATION_REPORT_MESSAGE);
+        allocationReport = parser.parse(String.format(TestData.FIXML_ALLOCATION_REPORT_MESSAGE, ALLOCATION_REPORT_ID));
     }
 
     @Test
     public void parses_allocation_id() throws Exception {
-        assertThat(allocationReport.getAllocationId()).isEqualTo("1234567");
+        assertThat(allocationReport.getAllocationId()).isEqualTo(ALLOCATION_REPORT_ID);
     }
 
     @Test
--- confirmation-sender/src/test/java/com/trading/ConfirmationSenderApplicationIntegrationTest.java
+++ confirmation-sender/src/test/java/com/trading/ConfirmationSenderApplicationIntegrationTest.java
@@ -2,21 +2,46 @@ package com.trading;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.jms.ConnectionFactory;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class ConfirmationSenderApplicationIntegrationTest {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private BrokerService brokerService;
+
+    @Before
+    public void setUp() throws Exception {
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+        brokerService = new BrokerService();
+        brokerService.addConnector("tcp://localhost:9999");
+        brokerService.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        brokerService.stop();
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+    }
 
     @Test
     public void consumes_incoming_message_and_send_confirmation() throws Exception {
@@ -32,10 +57,10 @@ public class ConfirmationSenderApplicationIntegrationTest {
                 session -> session.createTextMessage(allocationReportAsJson)
         );
 
-        TimeUnit.SECONDS.sleep(2);
+        TimeUnit.SECONDS.sleep(5);
 
         Confirmation confirmation = restTemplate.getForObject(
-                "https://confirmation-service.herokuapp.com/api/confirmation?id="
+                "http://confirmation-service.herokuapp.com/api/confirmation?id="
                         + allocationReport.getAllocationId(),
                 Confirmation.class
         );
@@ -51,7 +76,7 @@ public class ConfirmationSenderApplicationIntegrationTest {
     private ConnectionFactory connectionFactory() {
 
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL("http://activemq-nprogramming.rhcloud.com");
+        activeMQConnectionFactory.setBrokerURL("tcp://localhost:9999");
 
         SingleConnectionFactory factory = new SingleConnectionFactory();
         factory.setTargetConnectionFactory(activeMQConnectionFactory);
