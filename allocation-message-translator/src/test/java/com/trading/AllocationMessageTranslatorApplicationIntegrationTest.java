48cc81b92c1e646fa7aa676e1c11052cc81fec7f
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-21 19:25:47 +0100

    - make integration test to run against random id

--- allocation-message-translator/src/test/java/com/trading/AllocationMessageTranslatorApplicationIntegrationTest.java
+++ allocation-message-translator/src/test/java/com/trading/AllocationMessageTranslatorApplicationIntegrationTest.java
@@ -8,6 +8,8 @@ import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AllocationMessageTranslatorApplicationIntegrationTest {
@@ -18,17 +20,21 @@ public class AllocationMessageTranslatorApplicationIntegrationTest {
     public void consumes_incoming_message_and_sent_transfored_message_back_to_jms_server() throws Exception {
         AllocationMessageTranslatorApplication.main(new String[0]);
 
+        String allocationReportId = UUID.randomUUID().toString();
+
         JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
         jmsTemplate.send(
                 outgoingQueue(),
-                session -> session.createTextMessage(TestData.FIXML_ALLOCATION_REPORT_MESSAGE)
+                session -> session.createTextMessage(String.format(TestData.FIXML_ALLOCATION_REPORT_MESSAGE, allocationReportId))
         );
 
         String message = (String) jmsTemplate.receiveAndConvert(incomingQueue());
 
         AllocationReport allocationReport = objectMapper.readValue(message, AllocationReport.class);
 
-        assertThat(allocationReport).isEqualToComparingFieldByField(TestData.allocationReport());
+        AllocationReport expected = TestData.allocationReport();
+        expected.setAllocationId(allocationReportId);
+        assertThat(allocationReport).isEqualToComparingFieldByField(expected);
     }
 
     private String incomingQueue() {
