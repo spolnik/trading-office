02532992dc8b0c483a37ecc1fe1fcf664440c07a
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-21 08:38:59 +0100

    - add json serialization tests for InstrumentDetails

---
+++ trading-domain/src/test/java/com/trading/InstrumentDetailsSpec.java
@@ -0,0 +1,38 @@
+package com.trading;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InstrumentDetailsSpec {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void can_be_parsed_to_json_and_converted_back_to_object() throws Exception {
+
+        String instrumentDetailsAsJson = objectMapper.writeValueAsString(
+                instrumentDetails()
+        );
+
+        InstrumentDetails instrumentDetails = objectMapper.readValue(
+                instrumentDetailsAsJson, InstrumentDetails.class
+        );
+
+        assertThat(instrumentDetails).isEqualToComparingFieldByField(instrumentDetails());
+    }
+
+    private InstrumentDetails instrumentDetails() {
+
+        InstrumentDetails instrumentDetails = new InstrumentDetails();
+
+        instrumentDetails.setCusip("DUMMY_CUSIP");
+        instrumentDetails.setName("DUMMY_NAME");
+        instrumentDetails.setRic("DUMMY_RIC");
+        instrumentDetails.setSedol("DUMMY_SEDOL");
+        instrumentDetails.setTicker("DUMMY_SYMBOL");
+
+        return instrumentDetails;
+    }
+}
