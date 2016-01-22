2bb750efc4467b7a3ba230ae3357c81b464f819b
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-22 06:08:38 +0100

    - simplify jms code in confirmation sender

--- confirmation-sender/src/test/java/com/trading/ConfirmationSenderApplicationIntegrationTest.java
+++ confirmation-sender/src/test/java/com/trading/ConfirmationSenderApplicationIntegrationTest.java
@@ -8,11 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.jms.ConnectionFactory;
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,17 +23,15 @@ public class ConfirmationSenderApplicationIntegrationTest {
 
     @Before
     public void setUp() throws Exception {
-        FileSystemUtils.deleteRecursively(new File("activemq-data"));
-
         brokerService = new BrokerService();
         brokerService.addConnector("tcp://localhost:9999");
+        brokerService.setPersistent(false);
         brokerService.start();
     }
 
     @After
     public void tearDown() throws Exception {
         brokerService.stop();
-        FileSystemUtils.deleteRecursively(new File("activemq-data"));
     }
 
     @Test
@@ -61,7 +57,9 @@ public class ConfirmationSenderApplicationIntegrationTest {
         );
 
         allocationReport.setMessageStatus(MessageStatus.SENT);
-        assertThat(confirmation.getAllocationReport()).isEqualToComparingFieldByField(allocationReport);
+
+        assertThat(confirmation.getAllocationReport())
+                .isEqualToComparingFieldByField(allocationReport);
     }
 
     private String queue() {
@@ -70,11 +68,8 @@ public class ConfirmationSenderApplicationIntegrationTest {
 
     private ConnectionFactory connectionFactory() {
 
-        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL("tcp://localhost:9999");
-
-        SingleConnectionFactory factory = new SingleConnectionFactory();
-        factory.setTargetConnectionFactory(activeMQConnectionFactory);
-        return factory;
+        return new SingleConnectionFactory(
+                new ActiveMQConnectionFactory("tcp://localhost:9999")
+        );
     }
 }
