3da5396f75936fc22de601648202755ae5e67cca
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-29 20:00:08 +0100

    - remove old file based intrument repository implementation

--- instruments-service/src/test/java/com/trading/InstrumentsRepositorySpec.java
+++ instruments-service/src/test/java/com/trading/InstrumentsRepositorySpec.java
@@ -6,28 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class InstrumentsRepositorySpec {
 
-    private InstrumentsRepository repository = new FileBasedInstrumentsRepository();
-
-    @Test
-    public void can_query_by_sedol() throws Exception {
-
-        InstrumentDetails instrumentDetails = repository.queryBySedol("2000019");
-
-        assertThat(instrumentDetails.getTicker()).isEqualTo(
-                "AMZN"
-        );
-    }
-
-    @Test
-    public void returns_empty_instrument_details_if_invalid_id() throws Exception {
-
-        InstrumentDetails instrumentDetails = repository.queryBySedol("DUMMY");
-
-        assertThat(instrumentDetails).isEqualToComparingFieldByField(
-                InstrumentDetails.empty()
-        );
-    }
-
     @Test
     public void returns_instrument_data_based_on_sedol_id_through_open_figi_api() throws Exception {
         OpenFigiInstrumentsRepository repository = new OpenFigiInstrumentsRepository();
