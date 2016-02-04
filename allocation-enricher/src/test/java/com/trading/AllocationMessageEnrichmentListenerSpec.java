40e3d3a966f10f240a742a5cfb4a064bee655de0
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-02-04 18:48:36 +0100

    - add unit tests covering throwing exception in case of instrument details being null

---
+++ allocation-enricher/src/test/java/com/trading/AllocationMessageEnrichmentListenerSpec.java
@@ -0,0 +1,29 @@
+package com.trading;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AllocationMessageEnrichmentListenerSpec {
+
+    private InstrumentsApi instrumentsApi;
+    private AllocationMessageEnrichmentListener listener;
+
+    @Before
+    public void setUp() throws Exception {
+        instrumentsApi = mock(InstrumentsApi.class);
+        listener = new AllocationMessageEnrichmentListener(instrumentsApi);
+    }
+
+    @Test(expected = IOException.class)
+    public void throws_exception_if_cannot_return_instrument_details() throws Exception {
+        when(instrumentsApi.getInstrumentDetails(any(), any())).thenReturn(null);
+        listener.processAllocationReport("dummy");
+    }
+}
