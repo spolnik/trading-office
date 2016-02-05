f847fabf2da65a2c230e0b3f004311e2639f60ea
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-02-05 18:38:05 +0100

    - add counterparty service module - add csv file containing mic exchange codes with all related data - add skeletons for Exchange and Counterparty controllers

---
+++ counterparty-service/src/main/java/com/trading/CounterpartyController.java
@@ -0,0 +1,9 @@
+package com.trading;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CounterpartyController {
+}
