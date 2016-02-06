66f9a82a93e95ba6cf1cde6f96d1319c518a0d3e
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-02-06 15:47:33 +0100

    - add script to deploy all components

---
+++ deployAll.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function refresh {
+
+    git checkout $1
+    git merge master
+    git push
+}
+
+git checkout master
+git merge origin/master
+
+refresh heroku-instruments-service
+refresh heroku-counterparty-service
+refresh heroku-confirmation-service
+refresh heroku-finance-data-service
+refresh heroku-allocation-message-translator
+refresh heroku-allocation-enricher
+refresh heroku-confirmation-sender
+refresh coverity_can
+
+git checkout master
