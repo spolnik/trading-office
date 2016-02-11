baf5dfa2f04f0ec58014ddc0c863c84d940cfada
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-02-11 08:20:19 +0100

    -add missing unit tests covering joint points between instruments and counterparty api and the listener

--- allocation-enricher/src/test/java/com/trading/AllocationMessageEnrichmentListenerSpec.java
+++ allocation-enricher/src/test/java/com/trading/AllocationMessageEnrichmentListenerSpec.java
@@ -8,24 +8,78 @@ import java.io.IOException;
 import static com.trading.DomainObjectMapper.objectMapper;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class AllocationMessageEnrichmentListenerSpec {
 
     private InstrumentsApi instrumentsApi;
+    private CounterpartyApi counterpartyApi;
+
     private AllocationMessageEnrichmentListener listener;
 
     @Before
     public void setUp() throws Exception {
         instrumentsApi = mock(InstrumentsApi.class);
-        CounterpartyApi counterpartyApi = mock(CounterpartyApi.class);
+        counterpartyApi = mock(CounterpartyApi.class);
         listener = new AllocationMessageEnrichmentListener(instrumentsApi, counterpartyApi);
     }
 
     @Test(expected = IOException.class)
     public void throws_exception_if_cannot_return_instrument_details() throws Exception {
         when(instrumentsApi.getInstrumentDetails(any(), any())).thenReturn(null);
-        String json = objectMapper().toJson(TestData.allocationReport());
-        listener.processAllocationReport(json);
+        listener.processAllocationReport(allocationReportAsJson());
+    }
+
+    @Test
+    public void uses_instruments_api_to_get_instrument_details() throws Exception {
+        setupInstrumentsApi();
+
+        listener.processAllocationReport(allocationReportAsJson());
+        verify(instrumentsApi).getInstrumentDetails("2000019", InstrumentType.SEDOL);
+    }
+
+    @Test
+    public void uses_instruments_api_to_get_instrument() throws Exception {
+        setupInstrumentsApi();
+
+        listener.processAllocationReport(allocationReportAsJson());
+        verify(instrumentsApi).getInstrument("AMZN");
+    }
+
+    @Test
+    public void uses_counterparty_api_to_get_exchange() throws Exception {
+        setupInstrumentsApi();
+
+        listener.processAllocationReport(allocationReportAsJson());
+        verify(counterpartyApi).getExchange("XNAS");
+    }
+
+    @Test
+    public void uses_counterparty_api_to_get_counterparty() throws Exception {
+        setupInstrumentsApi();
+
+        listener.processAllocationReport(allocationReportAsJson());
+        verify(counterpartyApi).getParty("CUSTUS");
+    }
+
+    @Test
+    public void uses_counterparty_api_to_get_executing_party() throws Exception {
+        setupInstrumentsApi();
+
+        listener.processAllocationReport(allocationReportAsJson());
+        verify(counterpartyApi).getParty("TROF");
+    }
+
+    private String allocationReportAsJson() throws com.fasterxml.jackson.core.JsonProcessingException {
+        return objectMapper().toJson(TestData.allocationReport());
+    }
+
+    private void setupInstrumentsApi() {
+        InstrumentDetails instrumentDetails = mock(InstrumentDetails.class);
+
+        when(instrumentDetails.getTicker()).thenReturn("AMZN");
+
+        when(instrumentsApi.getInstrumentDetails(any(), any())).thenReturn(instrumentDetails);
     }
 }
