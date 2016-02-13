30a3d235ad051207a119bc9837f8c89168b1fe4e
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-02-13 13:25:06 +0100

    - add swagger api documentation to instruments service

--- instruments-service/src/main/java/com/trading/InstrumentsApplication.java
+++ instruments-service/src/main/java/com/trading/InstrumentsApplication.java
@@ -2,11 +2,44 @@ package com.trading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+        InstrumentsController.class
+})
 public class InstrumentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InstrumentsApplication.class, args);
+    }
+
+    @Bean
+    public Docket instrumentsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("instruments")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api/instruments.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Instruments REST Service")
+                .description("Instruments REST Service")
+                .contact("Jacek Spólnik")
+                .license("Apache License Version 2.0")
+                .version("1.0")
+                .build();
     }
 }
--- instruments-service/src/main/java/com/trading/InstrumentsController.java
+++ instruments-service/src/main/java/com/trading/InstrumentsController.java
@@ -1,8 +1,11 @@
 package com.trading;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +20,10 @@ class InstrumentsController {
         this.instrumentsRepository = instrumentsRepository;
     }
 
-    @RequestMapping("/instruments/sedol/{id}")
-    public InstrumentDetails getBySedol(@PathVariable String id) {
+    @ApiOperation(value = "getInstrumentDetails", nickname = "getInstrumentDetails", notes = "Example SEDOL: 2000019")
+    @RequestMapping(value = "/instruments/sedol/{id}", method = RequestMethod.GET)
+    @ApiParam(name = "id", example = "2000019", required = true)
+    public InstrumentDetails getInstrumentDetailsBySedol(@PathVariable String id) {
         return instrumentsRepository.queryBySedol(id);
     }
 }
