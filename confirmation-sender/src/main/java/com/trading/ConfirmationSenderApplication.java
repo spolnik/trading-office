f29eec19c34408b62984c4967dcd2eb218ea2577
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-03-02 20:02:33 +0100

    - add missing annotation

--- confirmation-sender/src/main/java/com/trading/ConfirmationSenderApplication.java
+++ confirmation-sender/src/main/java/com/trading/ConfirmationSenderApplication.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class ConfirmationSenderApplication {
 
     @Autowired
