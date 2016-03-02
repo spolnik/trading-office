64bd5d1639c17732f4f729cd0930f8729fd19e47
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-03-02 20:10:17 +0100

    - add header declaration (application/json)

--- confirmation-sender/src/main/java/com/trading/ConfirmationClient.java
+++ confirmation-sender/src/main/java/com/trading/ConfirmationClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient("CONFIRMATION-SERVICE")
 interface ConfirmationClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/api/confirmation")
+    @RequestMapping(method = RequestMethod.POST, value = "/api/confirmation",
+            consumes = "application/json")
     void send(Confirmation confirmation);
 }
