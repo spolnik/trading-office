b0486ad29bb7525a2a7eaa55275d6e81e84f1bc0
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-03-15 19:38:30 +0100

    - add simple app tu ping all heroku services

---
+++ src/test/groovy/com/trading/PingAllHerokuServicesInParallel.groovy
@@ -0,0 +1,41 @@
+package com.trading
+
+import groovyx.gpars.GParsPool
+import groovyx.net.http.RESTClient
+import org.slf4j.LoggerFactory
+
+class PingAllHerokuServicesInParallel {
+
+    static def log = LoggerFactory.getLogger(PingAllHerokuServicesInParallel.class)
+
+    def static void main(String[] args) {
+
+        GParsPool.withPool 2, {
+            def services = [
+                    new Tuple2<String, String>('eureka-server-staging', 'eureka-server'),
+                    new Tuple2<String, String>('confirmation-service-staging', 'confirmation-service'),
+                    new Tuple2<String, String>('market-data-service-staging', 'market-data-service'),
+                    new Tuple2<String, String>('counterparty-service-staging', 'counterparty-service'),
+                    new Tuple2<String, String>('trading-office-api-staging', 'trading-office-api'),
+                    new Tuple2<String, String>('allocation-receiver-staging', 'allocation-message-receiver'),
+                    new Tuple2<String, String>('allocation-enricher-staging', 'allocation-enricher'),
+                    new Tuple2<String, String>('confirmation-sender-staging', 'confirmation-sender')
+            ];
+
+            services.each {
+                this.&healthCheck.callAsync("http://${it.getFirst()}.herokuapp.com/");
+                this.&healthCheck.callAsync("http://${it.getSecond()}.herokuapp.com/");
+            }
+        }
+    }
+
+    static def healthCheck(String url) {
+
+        log.info("$url - checking...")
+        def status = new RESTClient(url)
+                .get(path: "health")
+                .responseData.diskSpace.status.toString()
+
+        log.info("$url - $status")
+    }
+}
